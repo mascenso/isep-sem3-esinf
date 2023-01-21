b2aa23b05a17b50bfa667565af091b3f0b9236af
ascensom <1210855@isep.ipp.pt>
2023-01-21 18:06:06 +0000

    [1 - Collections] Update solution of exercise 1 of 2020-01-23 exam

--- src/main/java/exames/Normal20.java
+++ src/main/java/exames/Normal20.java
@@ -16,42 +16,64 @@ public class Normal20 {
      * public static<K, E extends Comparable<E>> List<Pair<K,E>>
      * mergeLists(List<Pair<K,E>> A, List<Pair<K,E>> B)
      */
-    public static<K, E extends Comparable<E>> List<Map.Entry<K,E>> mergeLists(List<Map.Entry<K,E>> A, List<Map.Entry<K,E>> B){
+    public static<K, E extends Comparable<E>> List<Map.Entry<K,E>> mergeLists(List<Map.Entry<K,E>> A, List<Map.Entry<K,E>> B) {
+        List<Map.Entry<K,E>> result = new ArrayList<>();
+        int i = 0, j = 0;
 
-        List<Map.Entry<K,E>> lista = new ArrayList<>();
-        Map.Entry<K,E> temp;
+        while (i < A.size() && j < B.size()) {
+            Map.Entry<K,E> a = A.get(i);
+            Map.Entry<K,E> b = B.get(j);
 
-        for (int i=0; i < Math.min(A.size(), B.size()) ; i++)
-        {
-            //guardo o maior no temp e adiciono o menor
-            E valueA = A.get(i).getValue();
-            E valueB = B.get(i).getValue();
+            if (a.getValue().compareTo(b.getValue()) < 0) {
+                result.add(a);
+                i++;
 
-            if (valueA.compareTo(valueB) >= 0) {
-                temp = A.get(i);
-                lista.add(B.get(i));
+            } else if (a.getValue().compareTo(b.getValue()) > 0) {
+                result.add(b);
+                j++;
+
             } else {
-                temp = B.get(i);
-                lista.add(A.get(i));
+                result.add(a);
+                result.add(b);
+                i++;
+                j++;
             }
-            lista.add(temp);
         }
 
-        // add remaining elements from list A
-        for (int i = Math.min(A.size(), B.size()); i < A.size(); i++) {
-            lista.add(A.get(i));
-        }
-
-        // add remaining elements from list B
-        for (int i = Math.min(A.size(), B.size()); i < B.size(); i++) {
-            lista.add(B.get(i));
-        }
-
-        return lista;
+        for(i=i; i < A.size();i++)
+            result.add(A.get(i));
+        for(j=j; j < B.size();j++)
+            result.add(B.get(j));
+        return result;
     }
+
 
     public static void main(String[] args) {
         System.out.println("Exame - Normal 20\n");
+        //* Lista A: [<A,2>, <A,2>, <A,5>] Lista B: [<B,1>, <B,1>, <B,2>, <B,3>, <B,4>, <B,4>, <B,5>]
+        //* Lista Resultado: [<B,1>, <B,1>, <A,2>, <B,2>, <A,2>, <B,3>, <B,4>, <B,4>, <A,5>, <B,5>]
+
+        List<Map.Entry<String, Integer>> listaA = new ArrayList<>();
+        List<Map.Entry<String, Integer>> listaB = new ArrayList<>();
+
+        listaA.add(new AbstractMap.SimpleEntry<>("A", 2));
+        listaA.add(new AbstractMap.SimpleEntry<>("A", 2));
+        listaA.add(new AbstractMap.SimpleEntry<>("A", 5));
+
+        listaB.add(new AbstractMap.SimpleEntry<>("B", 1));
+        listaB.add(new AbstractMap.SimpleEntry<>("B", 1));
+        listaB.add(new AbstractMap.SimpleEntry<>("B", 2));
+        listaB.add(new AbstractMap.SimpleEntry<>("B", 3));
+        listaB.add(new AbstractMap.SimpleEntry<>("B", 4));
+        listaB.add(new AbstractMap.SimpleEntry<>("B", 4));
+        listaB.add(new AbstractMap.SimpleEntry<>("B", 5));
+
+        List<Map.Entry<String, Integer>> listaResultado = mergeLists(listaA, listaB);
+
+        System.out.println("Lista A: " + listaA);
+        System.out.println("Lista B: " + listaB);
+
+        System.out.println("Lista Resultado: " + listaResultado);
 
     }
 }
