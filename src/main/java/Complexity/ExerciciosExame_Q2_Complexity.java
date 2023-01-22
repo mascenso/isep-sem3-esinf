e360e3593614a5af0e9448b62616d874c1b6ef92
ascensom <1210855@isep.ipp.pt>
2023-01-22 17:29:51 +0000

    Code cleanup

--- src/main/java/Complexity/ExerciciosExame_Q2_Complexity.java
+++ src/main/java/Complexity/ExerciciosExame_Q2_Complexity.java
@@ -17,7 +17,7 @@ public class ExerciciosExame_Q2_Complexity {
         {
             for (int j = n; j > 1; j=j/2)
             {
-                System.out.print("c"); //this method runs n times
+                System.out.print(j + "_");
                 // something O(1)
             }
         }
@@ -34,7 +34,7 @@ public class ExerciciosExame_Q2_Complexity {
             int j=ls-1 ;
             while (i < j) {
 
-                System.out.print("y"); //this method runs n times
+                System.out.print("y");
 
                 int temp=a[i] ;
                 a[i]=a[j] ;
@@ -112,21 +112,21 @@ public class ExerciciosExame_Q2_Complexity {
     } // Total = O(n) + O log n = O(n)
 
     public static void main(String[] args) {
-        Integer[] arraySmall = {1,2,3,4,5,6,7,8,9,10};
-        Integer[] arrayBig = new Integer[10000000];
         long start;
         long end;
 
 
-        System.out.println(" Recurso 2022 - 24/02/2022");
-        start = System.nanoTime();
+        System.out.println("Recurso 2022 - 24/02/2022:");
+        //clearly is logn * logn, see the pattern
         complexity(1000000000);
-        end = System.nanoTime();
+
         System.out.println();
-        System.out.println("Time: " + (end - start) + " ns");
+
+        complexity(100);
 
 
 
+        System.out.println("\n");
         System.out.println("Especial 2022 - 08/09/2022");
         //Pode se ver q ele corre aproximadamente n vezes
         // para n = 10 -> ~10
@@ -138,6 +138,7 @@ public class ExerciciosExame_Q2_Complexity {
         System.out.println(Arrays.toString(a));
 
 
+        System.out.println("\n");
         System.out.println("Binary Search:");
         int[] b = new int[1000000000];
         //time
@@ -146,6 +147,7 @@ public class ExerciciosExame_Q2_Complexity {
         end = System.nanoTime();
         System.out.println("Time: " + (end - start) + " ms");
 
+        System.out.println("\n");
         System.out.println("Mistery:");
         //Para n = 100 printa 580 vezes
         //Para numeros muito grandes, ate da stack overflow
@@ -158,6 +160,7 @@ public class ExerciciosExame_Q2_Complexity {
         System.out.println("Time: " + (end - start) + " ms");
 
 
+        System.out.println("\n");
         System.out.println("Exemplo:");
         start = System.nanoTime();
         exemplo (new Integer[1], 100); // o array n ta la a fazer nd, o q interessa é o 2 argumento
