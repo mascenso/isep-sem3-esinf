bcdefb72b1ae86554c47ba9eb0c39e7df13dea88
ascensom <1210855@isep.ipp.pt>
2023-01-22 15:44:14 +0000

    Update class Complexity

--- src/main/java/Complexity/ExerciciosExame_Q2_Complexity.java
+++ src/main/java/Complexity/ExerciciosExame_Q2_Complexity.java
@@ -1,5 +1,4 @@
 package Complexity;
-import Trees.PL_Trees.Trees.SortingUtils;
 
 import java.util.Arrays;
 
@@ -18,6 +17,7 @@ public class ExerciciosExame_Q2_Complexity {
         {
             for (int j = n; j > 1; j=j/2)
             {
+                System.out.print("c"); //this method runs n times
                 // something O(1)
             }
         }
@@ -34,7 +34,7 @@ public class ExerciciosExame_Q2_Complexity {
             int j=ls-1 ;
             while (i < j) {
 
-                System.out.println("x"); //this method runs n times
+                System.out.print("y"); //this method runs n times
 
                 int temp=a[i] ;
                 a[i]=a[j] ;
@@ -49,6 +49,16 @@ public class ExerciciosExame_Q2_Complexity {
 
     /**
      * Binary Search exercise (not from exams)
+     * Binary Search is O(log n)
+     * n = 10 -> prints 4 times
+     * n = 100 -> prints 7 times
+     * n = 1000 -> prints 10 times
+     * n = 10000 -> prints 14 times
+     * n = 100000 -> prints 17 times
+     * n = 1000000 -> prints 20 times
+     * n = 10000000 -> prints 24 times
+     * note that log_2 (10000000) = 24
+     * look at image: src/main/resources/Complexity/binarySearch.jpeg
      * @param a
      * @param x
      */
@@ -56,7 +66,7 @@ public class ExerciciosExame_Q2_Complexity {
         int li = 0;
         int ls = a.length - 1;
         while (li <= ls) {
-            System.out.print("X");
+            System.out.print("B");
             int m = (li + ls) / 2;
             if (a[m] == x) {
                 System.out.println("Found");
@@ -85,21 +95,48 @@ public class ExerciciosExame_Q2_Complexity {
     } //Total: n * log n
 
     public static void main(String[] args) {
+        long start;
+        long end;
 
-        System.out.println(" Especial 2022 - 08/09/2022");
-        complexity(100);
+
+        System.out.println(" Recurso 2022 - 24/02/2022");
+        start = System.nanoTime();
+        complexity(1000000000);
+        end = System.nanoTime();
+        System.out.println();
+        System.out.println("Time: " + (end - start) + " ns");
+
+
+
+        System.out.println("Especial 2022 - 08/09/2022");
+        //Pode se ver q ele corre aproximadamente n vezes
+        // para n = 10 -> ~10
+        // para n = 100 -> ~100
+        // para n = 1000 -> ~1000
         Integer[] a = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
         complexity(a, 0, a.length);
+        System.out.println();
         System.out.println(Arrays.toString(a));
 
 
+        System.out.println("Binary Search:");
+        int[] b = new int[1000000000];
+        //time
+        start = System.nanoTime();
+        binarySearch(b, 7); //a nr that is not in the array
+        end = System.nanoTime();
+        System.out.println("Time: " + (end - start) + " ms");
 
-        int[] b = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-        binarySearch(b, 10);
-
-        // Para n = 100 printa 580 vezes
-        // Confirma complexidade O(n*log n)
-        mistery (100);
+        System.out.println("Mistery:");
+        //Para n = 100 printa 580 vezes
+        //Para numeros muito grandes, ate da stack overflow
+        //pcausa do nr exagerado de chamadas recursivas
+        //Confirma complexidade O(n*log n)
+        start = System.nanoTime();
+        mistery (1000);
+        end = System.nanoTime();
+        System.out.println();
+        System.out.println("Time: " + (end - start) + " ms");
 
     }
 }
